0d50a1a0a3ddc7a9bb9a36519819da7e225d5d77
avanim <axm1205@case.edu>
2023-01-19 22:30:04 -0500

    updated README, cleaned up code, added comments

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# text-editor
+# Text Editor
+
+## Description
+
+J.A.T.E. is Just Another Text Editor that runs in the browser and can also function offline. This text editor can be used to create and store notes or code snippets regardless of whether you are connected to the Internet so that you can retrieve these notes for later use. I built this project to learn how to use a web manifest for installability, service workers and cache API for offline functionality, and indexedDB for client-side data persistance in order to transform the given application into a progressive web application (PWA).
+
+## Usage
+
+Click on the URL to use the app:
+
+On the website, add content to the text editor. After entering content, click somewhere else not in the text editor to ensure the content gets automatically saved. You can also either click on the "Install Me!" button or the install icon in the search bar to download the web app to your desktop.
+
+## License
+
+MIT License. Please refer to the LICENSE in the repo.
+
+## How to Contribute
+
+If you would like to contribute to this project, please email me at: avani.muchhala@case.edu.
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,9 +12,9 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('Update database.');
+  // Connect to jate DB, create transaction, open object store, update with new content
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
@@ -23,16 +23,17 @@ export const putDb = async (content) => {
   console.log('Data saved to the database ', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// Gets all the content from the database
 export const getDb = async () => {
-  console.log('Get data from database.');
+  // Connect to jate DB, create transaction, open object store, get all saved content
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
-  console.log('result.value ', result);
-  return result;
-}
+  console.log('Content: ', result[0].content);
+  // Return all content stored on single object with id of 1
+  return result[0].content;
+};
 
 initdb();
